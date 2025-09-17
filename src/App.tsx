bc7f7a6734022b73774f7af55e2cea2c88d91ca7
Asendia Mayco <asendia@icloud.com>
2025-09-17 13:12:47 +0700

    Commit code

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useEffect, useRef, } from 'react';
+import { Sheet, SheetRef } from "react-modal-sheet";
 import './App.css';
 
 function App() {
+  const sheetRef = useRef<SheetRef>(null);
+  const snapPoints = [0, 100, 1];
+  useEffect(() => {
+    const searchParams = new URLSearchParams(window.location.search);
+    const isOpen = searchParams.get('open') === '1';
+    sheetRef.current?.snapTo(isOpen ? 2 : 1);
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Sheet ref={sheetRef} isOpen={true} onClose={() => {
+        sheetRef.current?.snapTo(1);
+      }} snapPoints={snapPoints} initialSnap={1}>
+        <Sheet.Container>
+          <Sheet.Header />
+          <Sheet.Content>
+            <div style={{ padding: 20 }}>
+              Hello, World!
+            </div>
+          </Sheet.Content>
+        </Sheet.Container>
+        <Sheet.Backdrop />
+      </Sheet>
     </div>
   );
 }
